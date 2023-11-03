5e0ffa6b42673f455165d2d722b684606459a0df
Jan-Graf <Jan.Graf@m-tec.com>
2023-11-03 15:47:51 +0100

    Completly updated code
    Made code faster and more efficient

--- Main.py
+++ Main.py
@@ -8,9 +8,6 @@ from plotly.subplots import make_subplots
 
 # define dataset directory based on file location
 dataset_dir = os.path.join(os.path.dirname(__file__), "Dataset")
-# store the raw data for each function to draw the functions
-raw_train_data: pd.DataFrame
-raw_ideal_data: pd.DataFrame
 
 # define the train functions
 train_functions = {}
@@ -39,7 +36,7 @@ def get_funcs(file_name: str) -> None:
             raise Exception("The file name has no or the wrong file extension.")
         # check if the file name is a name and no path --> raise exception if not
         if "/" in file_name or "\\" in file_name:
-            raise Exception("Only the name if the file is expected. No information about the file path.")
+            raise Exception("Only the name of the file is expected. No information about the file path.")
         
         # store the complete path to the file in a variable and check, if the file exists --> raise exception if not
         complete_path = os.path.join(dataset_dir, file_name)
@@ -48,62 +45,66 @@ def get_funcs(file_name: str) -> None:
         
         # read csv file and store it in a data frame
         data = pd.read_csv(complete_path, sep=",")
-        # store the raw data in DataFrames
-        if file_name == "train.csv":
-            global raw_train_data
-            raw_train_data = data
-        elif file_name == "ideal.csv":
-            global raw_ideal_data
-            raw_ideal_data = data
-
-        # get covariances and variances
-        cov = data.cov(ddof=0, numeric_only=True)
-        var = data.var(axis=0, skipna= True, ddof=0, numeric_only=True)
-        # get mean of x values
-        x_mean = data.loc[:, "x"].mean()
 
         for y_axes in list(data.columns.values):
             # skip the x-axis
             if y_axes == "x":
                 continue
-            # calculate the params for the function
-            b = cov.loc["x"][y_axes] / var["x"]
-            a = data.loc[:, y_axes].mean() - b * x_mean
 
-            # depending on the file, add function to dict
+            # calculate the params for the function with degree 1
+            coefficients, residuals, rank, singular_values, rcond = np.polyfit(data["x"], data[y_axes], 1, full=True)
+            
+            # define residuals
+            min_residuals = residuals[0]
+            # while the residuals are equal, the min residual has been updated --> another iteration
+            while min_residuals == residuals:
+                # get parameters for the function of the next degree
+                updated_coefficients, residuals, rank, singular_values, rcond = np.polyfit(data["x"], data[y_axes], len(coefficients), full=True)
+                
+                # if no residuals can be found, the function can't be fitted --> exit loop
+                if len(residuals) == 0:
+                    break
+
+                # use rounded residuals because if not really big functions will be returned (degree of 36 and coeffiecents like 2e-46)
+                if round(residuals[0], 3) < round(min_residuals, 3) and rank >= len(updated_coefficients) - 1:
+                    min_residuals = residuals[0]
+                    coefficients = updated_coefficients
+            
             if file_name == "train.csv":
-                train_functions[y_axes] = functions.train_function(a, b)
+                train_functions[y_axes] = functions.train_function(data[["x", y_axes]], coefficients)
             elif file_name == "ideal.csv":
-                ideal_functions[y_axes] = functions.linear_function(a, b)
+                ideal_functions[y_axes] = functions.function(data[["x", y_axes]], coefficients)
             else:
                 raise Exception("The function for the test data is not neccessary.")
-    except:
-        pass
+    except Exception as error:
+        # handle the exception
+        print(error)
+        exit()
 
 def set_ideal_functions():
+    '''
+    Find the ideal function for every training function
+    '''
     try:
-        # check if there are 4 training functions as expected in the dict --> raise exception if not
-        if len(train_functions) < 4:
-            raise Exception("There are not enough training functions")
-        elif len(train_functions) > 4:
-            raise Exception("There are too much training functions")
         # check if there are 50 ideal functions as expected in the dict --> raise exception if not
         if len(ideal_functions) < 50:
             raise Exception("There are not enough training functions")
         elif len(ideal_functions) > 50:
-            raise Exception("There are too muach training functions")
+            raise Exception("There are too much training functions")
         
         # iterate through all training functions
         for train_func in train_functions:
-             # default: set first ideal function as ideal function
-            train_functions[train_func].ideal_function = ideal_functions["y1"]
-
             # iterate through ideal functions
             for ideal_func in ideal_functions:
                 # check, if the current ideal function fits better to the training function as the actual one
                 train_functions[train_func].check_ideal_function(ideal_functions[ideal_func], ideal_func)
-    except:
-        pass
+            
+            # predict the values for the ideal function
+            ideal_functions[train_functions[train_func].ideal_no].predict_values()
+    except Exception as error:
+        # handle the exception
+        print(error)
+        exit()
 
 def map_test_function():
     try:
@@ -113,58 +114,46 @@ def map_test_function():
         elif len(train_functions) > 4:
             raise Exception("There are too much training functions")
         
-        # load test function
-        test_functions = pd.read_csv(os.path.join(dataset_dir, "test.csv"))
-        # load training data (again)
-        data = pd.read_csv(os.path.join(dataset_dir, "train.csv"), sep=",")
-        # iter through test functions
-        for index, row in test_functions.iterrows():
-            # get y value of test function
-            test_y = row["y"]
-
-            for train_func in train_functions:
-                # get y value of train function
-                train_y = data.loc[data["x"] == row["x"], train_func].values[0]
-                # if criterion is matched, add point to list
-                if abs(train_y - test_y) < math.sqrt(2):
-                    train_functions[train_func].mapped_points.append(functions.mapping_point(row["x"], train_y, train_y - test_y, train_functions[train_func].ideal_no))
-    except:
-        pass
+        # iterate through training functions
+        for train_func in train_functions:
+            # get ideal function
+            ideal_func = ideal_functions[train_functions[train_func].ideal_no]
+            # iterate through ideal values
+            for index, row in ideal_func.predicted_data.iterrows():
+                # get y deviation
+                delta_y = abs(row["y"] - train_functions[train_func].raw_data.at[index, "y"])
+                # if the deviation is smaller than zero add mapping point
+                if delta_y < math.sqrt(2):
+                    train_functions[train_func].mapped_points.append(functions.mapping_point(row["x"], row["y"], delta_y))
+    except Exception as error:
+        # handle the exception
+        print(error)
+        exit()
 
 def visualize_functions():
     # create a figure object
     fig = make_subplots(rows=2, cols=2, shared_xaxes=False, shared_yaxes=False, horizontal_spacing=0.1, vertical_spacing=0.1, subplot_titles=['Training Function y1', 'Training Function y2', 'Training Function y3', 'Training Function y4'])
     
+    index_mapping = {"y1": (1, 1), "y2": (1, 2), "y3": (2, 1), "y4": (2, 2)}
     # iterate through all training functions
     for train_func in train_functions:
         # get row and col index of the function
-        if train_func == "y1":
-            row_index = 1
-            col_index = 1
-        elif train_func == "y2":
-            row_index = 1
-            col_index = 2
-        elif train_func == "y3":
-            row_index = 2
-            col_index = 1
-        else:
-            row_index = 2
-            col_index = 2
+        row_index, col_index = index_mapping[train_func]
         
         # draw graph of the raw data
         fig.add_trace(
-            go.Scatter(x=raw_train_data["x"], y=raw_train_data[train_func], mode="lines", name=train_func, legendgroup=train_func),
+            go.Scatter(x=train_functions[train_func].raw_data["x"], y=train_functions[train_func].raw_data["y"], mode="lines", name=train_func),
             row=row_index,
             col=col_index
         )
 
-        # calucalte x and y values based on the ideal function
-        ideal_function_no = train_functions[train_func].ideal_no
-        x_values = np.linspace(raw_ideal_data["x"].min(), raw_ideal_data["x"].max(), raw_ideal_data["x"].count() + 1)
-        y_values = ideal_functions[ideal_function_no].slope * x_values + ideal_functions[ideal_function_no].y_intercept
+        # define ideal values
+        ideal_x = ideal_functions[train_functions[train_func].ideal_no].predicted_data["x"]
+        ideal_y = ideal_functions[train_functions[train_func].ideal_no].predicted_data["y"]
+        
         # add ideal function
         fig.add_trace(
-            go.Scatter(x=x_values, y=y_values, mode="lines", name="Ideal Function", legendgroup=train_func),
+            go.Scatter(x=ideal_x, y=ideal_y, mode="lines", name="Ideal Function for "+ train_func),
             row=row_index,
             col=col_index
         )
--- functions.py
+++ functions.py
@@ -1,72 +1,198 @@
 import math
+import pandas as pd
 
-class linear_function:
-    '''
-    The value for the y intercept
-    '''    
-    y_intercept: float = None
-    '''
-    The slope of the function
-    '''
-    slope: float = None
+# dict with unicodes for the numbers in superscript
+superscript = {0: '\u2070',
+               1: '\u00b9',
+               2: '\u00b2',
+               3: '\u00b3',
+               4: '\u2074',
+               5: '\u2075',
+               6: '\u2076',
+               7: '\u2077',
+               8: '\u2078',
+               9: '\u2079'
+               }
 
-    def __init__(self, a: float, b: float):
+class function:
+    '''
+    A simple class to store functions and its parameters
+    '''
+
+    raw_data: pd.DataFrame
+    '''
+    An empty DataFrame for the raw values
+    '''
+    coefficients: list = []
+    '''
+    A list of the coefficients of the function
+    '''
+    predicted_data: pd.DataFrame
+    '''
+    A data frame for the predicted values of the function
+    '''
+
+    def __init__(self, raw_data: pd.DataFrame, coefficients: list):
         '''
-        Initalize a new linear function of the form y = a + b * x
+        Initalize a new function
 
-        :param float a: The y intercept of the function
-        :param float b: The slope of the function
+        Args:
+            raw_data pd.DataFrame: A DataFrame containing the raw data
+            coeffiecents list: The coefficients of the functions            
         '''
-        self.y_intercept = a
-        self.slope = b
+        # set raw data
+        self.raw_data = raw_data
+        # rename column to only y
+        self.raw_data.columns.values[1] = "y"
+        # add coefficents to the list
+        self.coefficients = coefficients
+    
+    def predict_values(self):
+        '''
+        Get all predicted y-values
+        '''
+        # calcualte the y-values
+        y_values = []
+        for x in self.raw_data["x"]:
+            y_values.append([x, self._get_predicted_y(x)])
+        # set predicted values
+        self.predicted_data = pd.DataFrame(data=y_values, columns=["x", "y"])
 
+    def _get_predicted_y(self, x_value: float) -> float:
+        '''
+        Calculate the y-value for a given x-value
+
+        Args:
+            x_value float: The x-value to get the y-value for
+
+        Return:
+            float: The value for y at x
+        '''
+
+        predicted_y = 0.0
+        # enumerate through all coeffiecents and sum it up
+        for power, coefficent in enumerate(self.coefficients):
+            predicted_y += coefficent * (x_value ** (len(self.coefficients) - 1 - power))
+        return predicted_y
+    
     def __str__(self):
-        # return function in the form: y = a + b * x
-        return "y = " + str(self.y_intercept) + " + " + str(self.slope) + " * x"
+        '''
+        return function as string
+        '''
+        # get the degree of the fuction
+        degree = len(self.coefficients) - 1
 
-class train_function(linear_function):
+        result = "y = "
+        for i in range(degree, 1, -1):
+            # add every power to the function
+            result += self.__FloatToString(self.coefficients[(i + 1) * -1]) + "x" + self.__IntToSuperscript(i) + (" + " if self.coefficients[i * -1] >= 0 else " - ")
+        # add x and slope to function
+        result += self.__FloatToString(self.coefficients[-2]) + "x" + (" + " if self.coefficients[-1] >= 0 else " - ")
+        result += self.__FloatToString(self.coefficients[-1])
+            
+        return result
+    
+    def __FloatToString(self, number: float):
+        '''
+        Convert a float to a string with max. two decimal places and no sign
+
+        Args:
+            float: The float number to convert
+
+        Returns:
+            str: The given float as string
+        '''
+        return f"{abs(number):.2f}"
+    
+    def __IntToSuperscript(self, number: int):
+        '''
+        Convert a number to superscript
+
+        Args:
+            int: The int number to convert
+        
+        Returns:
+            str: A string with the given number as unicode superscript
+        '''
+
+        result = ""
+        # split the number in single integers and get superscript unicode
+        for single_int in list(map(int, str(number))):
+            result += superscript[single_int]
+        return result
+
+class train_function(function):
     '''
-    The ideal function for the training function
+    A class for the training functions to add ideal functions annd mapped points
     '''
-    ideal_function: linear_function
+    
+    ideal_no: str = None
     '''
     The number of the ideal function
     '''
-    ideal_no: str = None
+    predicted_data: pd.DataFrame
+    '''
+    DataFrame with the predicted values ideal function
+    '''
+    mapped_points: list = []
     '''
     The mapped points from the test set
     '''
-    mapped_points = []
+    __min_error: float = float("inf")
+    '''
+    The minimum error of the ideal function
+    '''
 
-    def __init__(self, a: float, b: float):
-        super().__init__(a, b)
+    def __init__(self, raw_data: pd.DataFrame, coefficents: list):
+        super().__init__(raw_data, coefficents)
+        self.mapped_points = []
         
-    def check_ideal_function(self, ideal_func: linear_function, func_no: str):
+    def check_ideal_function(self, ideal_func: function, func_no: str):
         '''
-        Compare the given function to ideal function. If the given function fits besser to the function itself, the ideal function gets updated.
+        Compare the given function to ideal function and checks the squarred sum error.
+
+        Args:
+            ideal_func: Ideal Function to compare with
+            func_no str: Number of the ideal function
         '''
-        # compare absolut deviations of slopes (given ideal function and actual ideal function)
-        # if the slope of the given function is less than the actual ideal function...
-        if abs(self.slope - ideal_func.slope) < abs(self.slope - self.ideal_function.slope):
-            # if the y intercept is less than the actual ideal function, update the ideal function
-            if abs(self.y_intercept - ideal_func.y_intercept) < abs(self.y_intercept - self.ideal_function.y_intercept):
-                self.ideal_function = ideal_func
-                self.ideal_no = func_no
-            # if the y intercept is greater but the slope is lower than the actual ideal function, check mapping criterion
-            # if the criterion is matched, update ideal function, because slope has more impact than the y intercept
-            elif abs(self.y_intercept - ideal_func.y_intercept) < math.sqrt(2):
-                self.ideal_function = func_no
-                self.ideal_no = func_no
+        error = 0
+        # get summed square error
+        for index, row in self.raw_data.iterrows():
+            error += (row["y"] - ideal_func._get_predicted_y(row["x"])) ** 2
+
+        # if the error is smaller, update the ideal function
+        if error < self.__min_error:
+            self.ideal_no = func_no
+            self.__min_error = error
 
 class mapping_point:
+    '''
+    Simple class for the mapped points
+    '''
+    
     x_value: float = None
+    '''
+    x-value of the point
+    '''
     y_value: float = None
+    '''
+    y-value of the point
+    '''
     delta_y: float = None
-    ideal_function_no: str = None
+    '''
+    The deviation of the raw y-value and the predicted y-value
+    '''
+    
+    def __init__(self, x: float, y: float, delta: float) -> None:
+        '''
+        Initialize a new Mapping Point
 
-    def __init__(self, x: float, y: float, delta: float, no: str) -> None:
+        Args:
+            x float: The x-value of the point
+            y float: The y-value of the point
+            delta float: The devation of the raw y-value and the predicted y-value
+        '''
         # set attributes
         self.x_value = x
         self.y_value = y
         self.delta_y = delta
-        self.ideal_function_no = no
